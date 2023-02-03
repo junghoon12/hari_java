4308c401dfcb008a5af9daf47929fc8560e218f9
임정훈 <121772106+junghoon12@users.noreply.github.com>
2023-02-03 16:21:18 +0900

    Add files via upload

---
+++ EmpDTO.java
@@ -0,0 +1,67 @@
+package model;
+
+// EMP 테이블의 컬럼명과 유사하게 멤버변수 선언
+
+public class EmpDTO {
+
+	private int empno;
+	private String ename;
+	private String job;
+	private int mgr;
+	private String hiredate;
+	private int sal;
+	private int comm;
+	private int deptno;
+	
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public int getMgr() {
+		return mgr;
+	}
+	public void setMgr(int mgr) {
+		this.mgr = mgr;
+	}
+	public String getHiredate() {
+		return hiredate;
+	}
+	public void setHiredate(String hiredate) {
+		this.hiredate = hiredate;
+	}
+	public int getSal() {
+		return sal;
+	}
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+	public int getComm() {
+		return comm;
+	}
+	public void setComm(int comm) {
+		this.comm = comm;
+	}
+	public int getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+	
+	
+	
+}
